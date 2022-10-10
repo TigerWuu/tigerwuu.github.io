751558da5228e52e209c568dafabd59a669a446a
TigerWuu <b06611032@ntu.edu.tw>
2022-10-10 12:42:02 +0800

    add search function

--- scripts/main.js
+++ scripts/main.js
@@ -1,27 +1,9 @@
-window.onload = function(){
-    var el = document.querySelectorAll('h1');
-    console.log(el);
-    // for(var i=0;i<el.length;i++){
-    //      el[i].style.color = 'white';
-    // }
-}
-
-window.addEventListener('click', function(e){
-    var l = document.querySelector('h1');
-    // l.textContent="About";
-    // console.log(e.clientX);
-    // console.log(e.clientY);
-    // e.clientX;
-    // e.clientY;
-    // var body = document.body;
-    // body.innerHTML="";
-})
-
-var user = {
-    name : "Bob",
-    greet : function(){
-        window.prompt("hi");
-    }
-}
-
-// setInterval(user.greet, 1000);
+// FIlter anything
+$(document).ready(function () {
+    $("#search").on("keyup", function () {
+      var value = $(this).val().toLowerCase();
+      $(".search *").filter(function () {
+        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+      });
+    });
+  });
